075b640d481de2a6dca3bfcfc277472b402c592e
r-paneva <r.paneva@gmail.com>
2019-03-17 16:52:42 +0200

    add some questions and answers

--- quizgame/src/main/java/com/ocadohackathon/quizgame/game/InMemoryQuestionAnswerRepository.java
+++ quizgame/src/main/java/com/ocadohackathon/quizgame/game/InMemoryQuestionAnswerRepository.java
@@ -26,6 +26,20 @@ public class InMemoryQuestionAnswerRepository {
     }
 
     private HashMap<Question, Answer> init() {
+
+        questionListHashMap.put(new Question(0,"Who was the first man to fly around the earth with a spaceship?",
+                new Answer(new ArrayList("Urii Gagarin","Alan Shepard","Jim Lovell","Peggy Whitson"),0)));
+
+        questionListHashMap.put(new Question(1,"On which hemisphere were the most dinosaur skeletons found?",
+                new Answer(new ArrayList("Africa","Europe","The northern hemisphere", "Australia"),2)));
+
+        questionListHashMap.put(new Question(2,"What color is cobalt?",
+                new Answer(new ArrayList("Pink","Purple","Blue","Green"),1)));
+
+        questionListHashMap.put(new Question(3,"Who invented vulcanized rubber?",
+                new Answer(new ArrayList("Bridgestone","Michelin","Pirreli","GoodYear"),3)));
+
+
         return questionListHashMap;
     }
 
@@ -40,4 +54,5 @@ public class InMemoryQuestionAnswerRepository {
     public boolean checkIfQuestionWasAsked(Integer questionId) {
         return !askedQuestionsIds.contains(questionId);
     }
+
 }
